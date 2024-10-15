e897bc50cb0e4065614c28cffa7801d3ef963186
Your name <you@example.com>
2024-10-15 20:16:52 +0200

    replaced cards by numbers in test

--- __tests__/queue.test.ts
+++ __tests__/queue.test.ts
@@ -1,34 +1,24 @@
 import { Queue } from "../structs/queue";
-import Cards from "../interface/cards";
-import exp from "constants";
 
 describe('Queue', () => {
-    let queue: Queue<Cards>;
+    let queue: Queue<number>;
 
     beforeEach(() => {
-        queue = new Queue<Cards>();
+        queue = new Queue<number>();
     });
 
     test('should peek the last item', () => {
-        const card: Cards = { color: 'red', number: 1 };
-        const card2: Cards = { color: 'red', number: 2 };
-        const card3: Cards = { color: 'red', number: 3 };
-
-        queue.enqueue(card);
-        queue.enqueue(card2);
-        queue.enqueue(card3);
-        expect(queue.peekLast()).toEqual(card3);
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        expect(queue.peekLast()).toEqual(3);
     });
 
     test('should peek the first item', () => {
-        const card: Cards = { color: 'red', number: 1 };
-        const card2: Cards = { color: 'red', number: 2 };
-        const card3: Cards = { color: 'red', number: 3 };
-
-        queue.enqueue(card);
-        queue.enqueue(card2);
-        queue.enqueue(card3);
-        expect(queue.peek()).toEqual(card);
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        expect(queue.peek()).toEqual(1);
     });
 
     test('should throw an error if len is 0 (peek & peekLast)', () => {
@@ -37,60 +27,43 @@ describe('Queue', () => {
     });
 
     test('should enqueue and dequeue items', () => {
-        const card: Cards = { color: 'red', number: 1 };
-        queue.enqueue(card);
-        expect(queue.dequeue()).toEqual(card);
+        queue.enqueue(1);
+        expect(queue.dequeue()).toEqual(1);
     });
 
     test('should dequeue first item and enqueue last item', () => {
-        const card: Cards = { color: 'red', number: 2 };
-        const card2: Cards = { color: 'green', number: 2 };
+        queue.enqueue(2);
+        queue.enqueue(3);
 
-        queue.enqueue(card);
-        queue.enqueue(card2);
-
-        expect(queue.peekLast()).toEqual(card2);
-        expect(queue.dequeue()).toEqual(card);
-    })
+        expect(queue.peekLast()).toEqual(3);
+        expect(queue.dequeue()).toEqual(2);
+    });
 
     test('should return size', () => {
-        const card: Cards = { color: 'red', number: 2 };
-        const card2: Cards = { color: 'green', number: 2 };
-
         expect(queue.getSize()).toEqual(0);
 
-        queue.enqueue(card);
-        queue.enqueue(card2);
+        queue.enqueue(2);
+        queue.enqueue(3);
 
         expect(queue.getSize()).toEqual(2);
-    })
+    });
 
     test('should check if isEmpty', () => {
-        const card: Cards = { color: 'red', number: 2 };
-        const card2: Cards = { color: 'green', number: 2 };
-
         expect(queue.isEmpty()).toEqual(true);
 
-        queue.enqueue(card);
-        queue.enqueue(card2);
+        queue.enqueue(2);
+        queue.enqueue(3);
 
         expect(queue.isEmpty()).toEqual(false);
-    })
+    });
 
     test('should shuffle the queue', () => {
-        const card: Cards = { color: 'red', number: 2 };
-        const card2: Cards = { color: 'green', number: 2 };
-        const card3: Cards = { color: 'red', number: 3 };
-        const card4: Cards = { color: 'yellow', number: 3 };
-        const card5: Cards = { color: 'red', number: 5 };
-        const card6: Cards = { color: 'red', number: 1 };
-
-        queue.enqueue(card);
-        queue.enqueue(card2);
-        queue.enqueue(card3);
-        queue.enqueue(card4);
-        queue.enqueue(card5);
-        queue.enqueue(card6);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+        queue.enqueue(5);
+        queue.enqueue(6);
+        queue.enqueue(1);
 
         const currentQueue = [...queue['data']];
 
@@ -98,10 +71,10 @@ describe('Queue', () => {
 
         expect(queue['data']).not.toEqual(currentQueue);
         expect(queue.getSize()).toEqual(6);
-    })
+    });
 
     test('deck should contain 108 cards', () => {
         queue.fillDeck();
         expect(queue.getSize()).toEqual(108);
-    })
+    });
 });
