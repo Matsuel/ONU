4809bf90b4215151a6f7ab262d229516b3f2e62f
flizze <alexandre.bel@hotmail.com>
2024-10-19 19:18:31 +0200

    displayed infos on _page.tsx

--- game.ts
+++ game.ts
@@ -1,4 +1,4 @@
-import { drawCard, isCardPlayable, isWinner, playCard } from "./cardsFunction";
+import { drawCard } from "./cardsFunction";
 import Cards from "./interface/cards";
 import Player from "./interface/player";
 import { LinkedList } from "./structs/linkedArray";
@@ -11,15 +11,9 @@ const pit = new Stack<Cards>();
 pit.push(deck.removeHead());
 
 const player1: Player = { name: 'Alexandre', cards: [] };
-const player2: Player = { name: 'Lukas', cards: [] };
-const player3: Player = { name: 'Matsuel', cards: [] };
-
-const players = [player1, player2, player3]
 
 for (let i = 0; i < 7; i++) {
     drawCard(player1, deck);
-    drawCard(player2, deck);
-    drawCard(player3, deck)
 }
 
 console.log('Checks if pit has a card')
@@ -27,34 +21,3 @@ console.log(pit.peek());
 
 console.log('\nChecks if player1 has 7 cards');
 console.log(player1);
-
-console.log('\nChecks if player2 has 7 cards');
-console.log(player2);
-
-function playerTurn(player: Player) {
-    for (let i = 0; i < player.cards.length; i++) {
-        if (isCardPlayable(player.cards[i], pit.peek())) {
-            console.log(`\n${player.name} played: ${player.cards[i].color} ${player.cards[i].number} ${player.cards[i].special}`);
-
-            playCard(player, pit, i);
-
-            if (isWinner(player)) {
-                console.log(`\n${player.name} has won !`);
-                game = false;
-            } 
-
-            break;
-        }
-
-        drawCard(player, deck);
-        console.log(`\n${player.name} drew a card.`)
-    }
-}
-
-let game = true;
-
-while (game) {
-    players.forEach(player => {
-        playerTurn(player);
-    });
-}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,54 @@
-import "@/styles/globals.css";
+import { LinkedList } from "../../structs/linkedArray";
+import { Stack } from "../../structs/stack";
+import Cards from "../../interface/cards";
+import Player from "../../interface/player";
+import { useEffect, useState } from "react";
 
 export default function App() {
-  return (
-    <div>
-      <h1>App</h1>
-    </div>
-  )
+    const [player1, setPlayer1] = useState<Player | null>(null);
+    const [player2, setPlayer2] = useState<Player | null>(null);
+
+    const [deck, setDeck] = useState<LinkedList<Cards> | null>(null);
+    const [pit, setPit] = useState<Stack<Cards> | null>(null);
+
+    useEffect(() => {
+        const newDeck = new LinkedList<Cards>();
+        newDeck.fillDeck();
+
+        const newPit = new Stack<Cards>();
+        const firstCard = newDeck.removeHead();
+        newPit.push(firstCard);
+
+        const p1: Player = { name: 'Alexandre', cards: [{}] };
+        const p2: Player = { name: 'Alexandre', cards: [{}] };
+
+        for (let i = 0; i < 7; i++) {
+            p1.cards.push(newDeck.removeHead());
+            p2.cards.push(newDeck.removeHead());
+        }
+
+        setPit(newPit);
+        setDeck(newDeck);
+        setPlayer1(p1);
+        setPlayer2(p2);
+    }, []);
+
+    return (
+        <pre>
+            {JSON.stringify(player1, null, 4)}
+            <br />
+            <br />
+            <br />
+            {JSON.stringify(player2, null, 4)}
+            <br />
+            <br />
+            <br />
+            Deck size: {deck?.getSize() || 0}
+            <br />
+            <br />
+            <br />
+            Pit size: {pit?.getSize() || 0}
+        </pre>
+    );
 }
+
