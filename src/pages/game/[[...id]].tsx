b23ff0595d90d7e0999af47bed64a03cff0e9d1c
Matsuel <matsuel0308@gmail.com>
2024-11-13 11:38:55 +0100

    Add timer for player play 30 seconds

--- src/pages/game/[[...id]].tsx
+++ src/pages/game/[[...id]].tsx
@@ -9,6 +9,7 @@ import Players from "@/components/Player";
 import Pit from "@/components/Pit";
 import { socket } from "../_app";
 import { useRouter } from "next/router";
+import { drawCard } from "../../../cardsFunction";
 
 export default function Game() {
   const router = useRouter();
@@ -22,7 +23,9 @@ export default function Game() {
   const [isTurnDirectionClockwise, setIsTurnDirectionClockwise] = useState(true);
   const [nmbCardsToDraw, setNmbCardsToDraw] = useState(0);
   const [uuid, setUuid] = useState("");
-  
+  const [loading, setLoading] = useState(true);
+  const [timer, setTimer] = useState(30);
+
   // TODO:
   // - Ajouter toutes les props de base dans la partie lors de sa création isTurnDirectionClockwise, nmbCardsToDraw
 
@@ -36,7 +39,10 @@ export default function Game() {
   }, []);
 
   useEffect(() => {
-    if (id) socket.emit("getGame", { id: id[0], uuid });
+    if (id) {
+      setLoading(true);
+      socket.emit("getGame", { id: id[0], uuid });
+    }
   }, [uuid, id]);
 
   useEffect(() => {
@@ -47,6 +53,7 @@ export default function Game() {
       setPit(new Stack(data.game.pit.stack));
       setDeck(newDeck);
       setPlayers(data.game.players as Player[]);
+      setLoading(false);
     });
 
     socket.on("gameOver", (data) => {
@@ -56,10 +63,33 @@ export default function Game() {
     });
   }, []);
 
-  if (!id) return <div>Loading...</div>;
+  useEffect(() => {
+    if (!uuid || players.length === 0 || !id) return;
+    if (players[playerTurn].uuid === uuid) {
+      const interval = setInterval(() => {
+        setTimer((prev) => {
+          if (prev === 0) {
+            drawCard(deck, pit, players, playerTurn, 1, id[0] as string);
+            clearInterval(interval);
+            return 30;
+          }
+          return prev - 1;
+        });
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+
+  }, [playerTurn, players, uuid, id]);
+
+  if (!id || !uuid || loading) return <div>Loading...</div>;
 
   return (
     <div className="flex flex-col bg-black w-screen min-h-screen text-white">
+      {players[playerTurn].uuid === uuid && (
+        <div className="bg-white text-black">
+          <h1>{timer}</h1>
+        </div>
+      )}
       <Players
         uuid={id[0] as string}
         players={players}
