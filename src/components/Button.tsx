3a866ca502127892f2c4717fdd0b8d256bb78d93
Matsuel <matsuel0308@gmail.com>
2024-11-14 09:52:32 +0100

    create customLink component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,16 @@
+import { cn } from "@/utils/cn";
+
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     label: string
 }
 
-const Button = ({label, ...props} : ButtonProps) => {
+const Button = ({
+    label,
+    className,
+    ...props
+}: ButtonProps) => {
     return (
-        <button {...props} className='py-4 px-2 rounded-lg bg-gradient-to-l from-[#FF4201] to-[#FF6603]'>
+        <button {...props} className={cn(`py-4 px-2 rounded-lg bg-gradient-to-l from-[#FF4201] to-[#FF6603]`, className)}>
             {label}
         </button>
     )
