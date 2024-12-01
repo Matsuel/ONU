76455617c72bf619c287cd9470a2724b08e3a183
Alexandre Bel <46611703+FLIzze@users.noreply.github.com>
2024-12-01 14:40:51 +0100

    Delete src directory

--- src/constantes/colors.ts
+++
@@ -1,3 +0,0 @@
-export const colors = ["red", "blue", "green", "yellow"]
-export const colorsCards = { "red": "r", "green": "g", "blue": "b", "yellow": "y" }
-export const colorsHex = { "r": "#ff0000", "g": "#00ff00", "b": "#0000ff", "y": "#ffff00" }
