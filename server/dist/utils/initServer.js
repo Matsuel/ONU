b98023418b56c8998896e97dd2ae7150b4159582
flizze <alexandre.bel@hotmail.com>
2024-12-02 10:41:44 +0100

    remove cors

--- server/dist/utils/initServer.js
+++ server/dist/utils/initServer.js
@@ -1,24 +1,14 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.initServer = void 0;
 require("dotenv/config");
 const http_1 = require("http");
-const express_1 = __importDefault(require("express"));
 const socket_io_1 = require("socket.io");
-const cors_1 = __importDefault(require("cors"));
 const initServer = () => {
-    const app = (0, express_1.default)();
-    app.use((0, cors_1.default)({
-        origin: "*",
-        methods: ["GET", "POST"],
-    }));
-    const server = (0, http_1.createServer)(app);
+    const server = (0, http_1.createServer)();
     const io = new socket_io_1.Server(server, {
         cors: {
-            origin: "*",
+            origin: "onu.alexandrebel.me",
             methods: ["GET", "POST"],
         },
     });
