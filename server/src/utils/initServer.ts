8291bce6c185b2e333b233403b662034ec0b73d9
flizze <alexandre.bel@hotmail.com>
2024-12-02 10:41:35 +0100

    remove cors

--- server/src/utils/initServer.ts
+++ server/src/utils/initServer.ts
@@ -1,24 +1,15 @@
 import "dotenv/config";
 import { createServer } from "http";
-import express from "express";
 import { Server } from "socket.io";
-import cors from "cors";
 
 export const initServer = (): {
     io: Server;
 } => {
-    const app = express();
-
-    app.use(cors({
-        origin: "*", 
-        methods: ["GET", "POST"], 
-    }));
-
-    const server = createServer(app);
+    const server = createServer();
 
     const io = new Server(server, {
         cors: {
-            origin: "*", 
+            origin: "onu.alexandrebel.me", 
             methods: ["GET", "POST"],
         },
     });
