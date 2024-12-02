d020ba627bb62cda1012bc38149aa86606d40daa
flizze <alexandre.bel@hotmail.com>
2024-12-02 14:39:49 +0100

    server

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -16,4 +16,4 @@ export default function App({
     );
 }
 
-export const socket = io(`http://localhost:8000`);
+export const socket = io(`https://server.onu.alexandrebel.me`);
--- server/src/utils/initServer.ts
+++ server/src/utils/initServer.ts
@@ -1,27 +1,13 @@
 import "dotenv/config";
 import { createServer } from "http";
-import express from "express";
 import { Server } from "socket.io";
-import cors from "cors";
 
 export const initServer = (): {
     io: Server;
 } => {
-    const app = express();
+    const server = createServer();
 
-    app.use(cors({
-        origin: "*",
-        methods: ["GET", "POST"],
-    }));
-
-    const server = createServer(app);
-
-    const io = new Server(server, {
-        cors: {
-            origin: "*",
-            methods: ["GET", "POST"],
-        },
-    });
+    const io = new Server();
 
     const port = process.env.NEXT_PUBLIC_PORT
     server.listen(port, () => {
